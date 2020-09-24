77c80d7b79697d31e099f79435e8155b98a0820f
StarDust <dmvstar.devel@gmail.com>
2020-09-24 13:51:06 +0300

    Add trans script + flows

--- src/make_f.sh
+++ src/make_f.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-cat index.html | sed 's:<script type="text/javascript" src="main.js"></script>:<script src="/public/meest/ui/main.js"></script>:' > flows/index.html
+cp index.html flows/index_l.html
+cat index.html | sed 's:<script type="text/javascript" src="main.js"></script>:<script src="/meestua/ui/main.js"></script>:' > flows/index_f.html
 cat main.js | sed 's:var modeUrl = 1;:var modeUrl = 3;:' > flows/main.js
  
