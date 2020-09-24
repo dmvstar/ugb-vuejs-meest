3be07a3cf783b5f354520d7b27f06abed0da46a8
StarDust <dmvstar.devel@gmail.com>
2020-09-24 13:14:30 +0300

    Add trans script + flows

--- src/make_f.sh
+++ src/make_f.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-cat $1 | sed 's:<script type="text/javascript" src="main.js"></script>:<script src="/public/meest/ui/main.js"></script>:'
+cat index.html | sed 's:<script type="text/javascript" src="main.js"></script>:<script src="/public/meest/ui/main.js"></script>:' > flows/index.html
+cat main.js | sed 's:var modeUrl = 1;:var modeUrl = 3;:' > flows/main.js
+ 
