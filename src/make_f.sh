9ba0a80aa9004be51a609614878ce40c18438d40
StarDust <dmvstar.devel@gmail.com>
2020-10-01 16:10:33 +0300

    Add login form check + script

--- src/make_f.sh
+++ src/make_f.sh
@@ -2,4 +2,5 @@
 cp index.html flows/index_l.html
 cat index.html | sed 's:<script type="text/javascript" src="main.js"></script>:<script src="/meestua/ui/main.js"></script>:' > flows/index_f.html
 cat main.js | sed 's:var modeUrl = 1;:var modeUrl = 3;:' > flows/main.js
+cat login.html | sed 's:"index.html","_self":"/meestua/ui/index.html","_self":' > flows/login_f.html
  
