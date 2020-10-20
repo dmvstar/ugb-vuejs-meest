c4c9d499c962836fae25af31da6cee8815f09c78
StarDust <dmvstar.devel@gmail.com>
2020-10-20 14:45:12 +0300

    add face images

---
+++ src/post/random_face.js
@@ -0,0 +1,16 @@
+function rr() { 
+
+    var i = Math.floor(1 + Math.random() * 5);
+    return i;
+
+}
+
+console.log( rr() );
+console.log( rr() );
+console.log( rr() );
+console.log( rr() );
+console.log( rr() );
+console.log( rr() );
+console.log( rr() );
+console.log( rr() );
+
