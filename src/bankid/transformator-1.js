3a6887ce8d7a45a2ebe9fa189aff6e78cbabc925
StarDust <dmvstar.devel@gmail.com>
2020-11-14 11:08:48 +0200

    transformator prepare convert data

--- src/bankid/transformator-1.js
+++ src/bankid/transformator-1.js
@@ -17,12 +17,70 @@ var bankid_transform = {};
     Operations
 }
 */
-
 //console.log(bankid_mapping);
-
+bankid_transform.Client = [];
+bankid_transform.Individual = [];
+bankid_transform.Identifications = [];
+bankid_transform.Addresses = [];
+bankid_transform.Communications = [];
+bankid_transform.Properties = [];
+bankid_transform.Scans = [];
+bankid_transform.Operations = [];
 for(o of bankid_mapping) {
     console.log(o);
+    var item = {};
+    switch(o.webbank.block) {
+        case "Client": {
+            item = {
+                "webbank": {                    
+                    "code": o.webbank.code,
+                    "type": o.webbank.type,
+                    "default": o.webbank.default
+                },
+                "bankid": {
+                    "block": o.bankid.block,
+                    "code": o.bankid.code,
+                    "type": o.bankid.type
+                },
+                "value": bankid_client[o.bankid.block][o.bankid.code]
+            };
+            bankid_transform.Client.push(item);
+        } 
+        break;
+        case "Individual": {
+
+        } 
+        break;
+        case "Identifications": {
+
+        } 
+        break;
+        case "Addresses": {
+
+        } 
+        break;
+        case "Communications": {
+
+        } 
+        break;
+        case "Properties": {
+
+        } 
+        break;
+        case "Scans": {
+
+        } 
+        break;
+        case "Operations": {
+
+        } 
+        break;
+    }
+    
+
 }
+
+console.log(bankid_transform);
 
 
 
