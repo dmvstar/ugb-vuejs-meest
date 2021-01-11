ead7b2fc4b2bd9c615175e61f81088c600fd23c2
StarDust <dmvstar.devel@gmail.com>
2021-01-11 21:57:53 +0200

    Add curl for get refs

---
+++ src/bankid/logs/test-nr-gw-getDataReferenceValues.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+curl -v -XGET \
+-H "Content-type:application/json" \
+-H "x-auth-token:PTzQlEIYZVslkOyzKh41cJCfJCSuhJJ8" \
+-H "x-get-geturl:https://nr-clients.dev.ukrgasaws.com/" \
+-H "x-get-pathto:scrooge/webbank/dictionary/" \
+-H "x-get-method:data_refs_values" \
+-H "x-get-params:catalogCode=PROFIT_WAY" \
+https://nr-gateway.dev.ukrgasaws.com/9118aabf34299ead9f57921edb7c8209/
+
+
+#var atoken = msg.payload.atoken;
+#var geturl = msg.payload.geturl;
+#var pathto = msg.payload.pathto;
+#var method = msg.payload.method;
+#var params = msg.payload.params;
+
