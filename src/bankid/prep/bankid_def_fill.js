27ae2e45a349de6bcd11f3ab4f3101332228bbba
StarDust <dmvstar.devel@gmail.com>
2021-01-25 15:41:21 +0200

    Update translit mapping

--- src/bankid/prep/bankid_def_fill.js
+++ src/bankid/prep/bankid_def_fill.js
@@ -1,5 +1,44 @@
-var defaults ={
-    "extends": {
+var defaults = {
+    "extends": {    
+        "countryCitizen": "804",
+        "maritalStatus": "1",
+        "uaResident": "1",
+        "codOblRee": "",
+        "flagTopLevelRisk": "0",
+        "workInfo": "Безробітний",
+        "birthCountry": "804",
+        "birthPlace": "Україна",
+        "profitWay": "14430",
+        "profitSrc": "16",
+        "ownFundsAmount": "1",
+        "ownFundsSrc": "9",
+        "monthAmouunt": "1",
+        "realEstateAvail": "1",
+        "movePropAvail": "1",
+        "otherBankAccount": "3",
+        "otherBankCredit": "0",
+        "otherBankDepo": "0",
+        "costPaperAvail": "Відсутні",
+        "planUseServices": "44652",
+        "ugbRelatePers": "2",
+        "flagPEPs": "0",
+        "statusBusiness": "5",
+        "usaChiisares": "0",
+        "typeCli": "3",
+        "photo25": "1",
+        "photo45": "1",
+        "verifyDate": "now",
+        "signDate": "now",
+        "education": "2",
+        "forTaxStatus": "0",
+        "verifyType": "3",
+        "reputation": "Задовільний",
+        "countryCitisien": "804",
+        "greenCard": "1",
+        "registryUSA": "0",
+        "statusFATCA": "0"
+        },
+    "extends0": {
         "countryCitizen": "804",
         "profitWay": "14300",
         "profitSrc": "1",
@@ -27,6 +66,7 @@ var defaults ={
         "statusFATCA": "3"
     }
 };
+
 var client   = {
     "person": {
         "type": "physical",
@@ -111,11 +151,47 @@ var client   = {
         }
     ]
 };
+
+function trans_date_to(date) {
+    var ret = date;
+    // from DD.MM.YYYY
+    // to 1982-05-03T00:00:00
+    if (date.length > 0) {
+        var parts = date.split('.');
+        ret = parts[2] + '-' + parts[1] + '-' + parts[0] + 'T00:00:00';
+    }
+    return ret;
+}
+function trans_date_fr(date) {
+    var ret = date;
+    // from 1982-05-03T00:00:00
+    // to DD.MM.YYYY
+    if (date.length > 0) {
+        var parts = date.split('-');
+        ret = parts[2].substring(0,2) + '.' + parts[1] + '.' + parts[0];
+    }
+    return ret;
+}
+function trans_date_date(date) {
+    var ret = date;
+    // from 2021-01-25T12:56:12.495Z 1982-05-03T00:00:00
+    // to 2021-01-25T00:00:00
+    if (date.length > 0) {
+        var parts = date.split('T');
+        ret = parts[0] + 'T00:00:00';
+    }
+    return ret;
+}
+
 console.log(client.extends);
 for (d in defaults.extends) {
     if(client.extends[d] === undefined)
-        client.extends[d] = defaults.extends[d];
+        if(defaults.extends[d] === "now")
+            client.extends[d] = trans_date_date(new Date().toISOString());
+        else
+            client.extends[d] = defaults.extends[d];
 }
 console.log(client.extends);
+
 
 
