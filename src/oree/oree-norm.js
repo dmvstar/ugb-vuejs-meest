0eaec9d27cb4d86e49813d920e01717841c5ee74
StarDust <dmvstar.devel@gmail.com>
2021-02-12 23:16:09 +0200

    Update OREE

---
+++ src/oree/oree-norm.js
@@ -0,0 +1,32 @@
+
+function normaliseJsonObject(jsonObject, out, akey){
+    for(var key in jsonObject){
+        if (typeof jsonObject[key] === 'object') {
+            //console.log("o  "+akey + " " + key);
+            if( key !== "$" ) {
+                out[key] = {};
+                normaliseJsonObject(jsonObject[key], out, key);
+            } else {
+                normaliseJsonObject(jsonObject[key], out, akey);
+            }
+            
+        } else{
+            //console.log("v  "+akey + " " + key + ": " + jsonObject[key]);
+            var skey = key;
+            if( key === "_") skey = "result";
+            if( key !== "$" ) out[akey][skey] = jsonObject[key];
+        }
+    }
+};
+
+// MAIN --------------------------------------------------------------------------------
+var resn = require('./oree-no.json');
+var out = {};
+normaliseJsonObject( resn, out);
+console.log( JSON.stringify(out, null, 4) );
+
+var reso = require('./oree-ok.json');
+out = {};
+normaliseJsonObject( reso, out);
+console.log( JSON.stringify(out, null, 4) );
+// MAIN --------------------------------------------------------------------------------
