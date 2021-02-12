fd75644edc2ea500f3c1b95a59892353519f6cc8
StarDust <dmvstar.devel@gmail.com>
2021-02-12 10:05:31 +0200

    Add UGBKafkaConsumer

---
+++ src/bankid/exam/UGBKafkaConsumer.java
@@ -0,0 +1,63 @@
+// https://dzone.com/articles/writing-a-kafka-consumer-in-java
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Collections;
+import java.util.Properties;
+
+public class UGBKafkaConsumer {
+
+    private final static String TOPIC = "LS-DM-CardStateEvents";
+    private final static String BOOTSTRAP_SERVERS = "10.13.46.2:9092, 10.13.46.3:9092, 10.13.46.25:9092";
+
+    private static Consumer<Long, String> createConsumer() {
+        final Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "UGBKafkaConsumer");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        // Create the consumer using props.
+        final Consumer<Long, String> consumer = new KafkaConsumer<>(props);
+
+        // Subscribe to the topic.
+        consumer.subscribe(Collections.singletonList(TOPIC));
+        return consumer;
+    }
+
+    static void runConsumer() throws InterruptedException {
+        final Consumer<Long, String> consumer = createConsumer();
+
+        final int giveUp = 100;
+        int noRecordsCount = 0;
+
+        while (true) {
+            final ConsumerRecords<Long, String> consumerRecords = consumer.poll(1000);
+
+            if (consumerRecords.count() == 0) {
+                noRecordsCount++;
+                if (noRecordsCount > giveUp)
+                    break;
+                else
+                    continue;
+            }
+
+            consumerRecords.forEach(record -> {
+                System.out.printf("Consumer Record:(%d, %s, %d, %d)\n", record.key(), record.value(),
+                        record.partition(), record.offset());
+            });
+
+            consumer.commitAsync();
+        }
+        consumer.close();
+        System.out.println("DONE");
+    }
+
+    public static void main(String... args) throws Exception {
+        runConsumer();
+    }
+
+}
