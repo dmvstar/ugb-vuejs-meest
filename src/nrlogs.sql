451026f833ad148c1f15bfe7507fecf6f34b25b8
StarDust <dmvstar.devel@gmail.com>
2021-02-13 16:19:53 +0200

    Update OREE

--- src/nrlogs.sql
+++
@@ -1,9 +0,0 @@
-SELECT id, date, level, source, message, exception, url
-	FROM public.nrlogs 
-	WHERE 1=1
-	AND source like '%PRO) Transgen XML%' 
-	AND level like 'DEBUG'
-	AND message like '%12858889329%' 
-	--AND date >= DATE(NOW()) --'2020-03-06'
-	ORDER BY ID DESC
-	LIMIT 100;
--- src/oree/oree-norm.js
+++ src/oree/oree-norm.js
@@ -1,25 +1,35 @@
-
-function normalise0(obj, aout, pkey){
+function normalise0(obj, aout, pkey) {
     if (typeof obj === 'object') {
         ret = {};
-        for(var key in obj){
+        for (var key in obj) {
             if (typeof obj[key] === 'object') {
-                console.log("o  "+pkey + " " + key);
-                if( key !== "$" ) {
+                console.log("o  " + pkey + " " + key);
+                if (key !== "$") {
                     aout[key] = {};
                     ret[key] = {};
                     console.log(ret);
                     ret = normalise(obj[key], aout, key);
                 } else {
                     ret = normalise(obj[key], aout, pkey);
-                }                
-            } else{
-                console.log("v  "+pkey + " " + key + ": " + obj[key]);
+                }
+            } else {
+                console.log("v  " + pkey + " " + key + ": " + obj[key]);
                 console.log(ret);
                 var skey = key;
-                if( key === "_")  skey = "result";
-                if( key !== "$" ) aout[pkey][skey] = obj[key];
-                if( key !== "$" ) {ret[pkey] = {}; ret[pkey][skey] = obj[key];}
+                if (key === "_") 
+                    skey = "result";
+                
+
+
+                if (key !== "$") 
+                    aout[pkey][skey] = obj[key];
+                
+
+
+                if (key !== "$") {
+                    ret[pkey] = {};
+                    ret[pkey][skey] = obj[key];
+                }
             }
         }
         return ret;
@@ -28,9 +38,40 @@ function normalise0(obj, aout, pkey){
     }
 };
 
+
+function normalise1(obj, pkey, pret) {
+    ret = {};
+    for (var key in obj) {
+        if (typeof obj[key] === 'object') {
+            console.log("o  " + pkey + " " + key);
+            if (key !== "$") { // if (ret === undefined ) ret = {}
+                if (pkey === undefined) { // console.log(">", key, ret);
+                    ret[key] = normalise(obj[key], key, ret);
+                    console.log("<", key, ret);
+                } else { // ret[pkey] = {};
+                    ret[key] = normalise(obj[key], key, ret);
+                    console.log(">", ret);
+                    // ret[pkey][key] = normalise(obj[key],  key,  ret);
+                }
+            } else {
+                console.log(" $", pret);
+                ret[key] = normalise(obj[key], key, ret);
+                // pret[key] = normalise(obj[key], pkey, ret);
+            }
+        } else {
+            console.log("  v  " + pkey + " " + key + ": " + obj[key]);
+            var skey = key;
+            // if( key === "_")  skey = "result";
+            // if( key !== "$" ) { ret[skey] = obj[key]; }
+            console.log(ret);
+        }
+    }
+    return ret;
+}
+
 function simplify(obj) {
     if (Object.prototype.toString.call(obj) == "[object Array]") {
-        var out = []; 
+        var out = [];
         if (obj.length > 1) {
             for (var i in obj) {
                 out[i] = simplify(obj[i]);
@@ -42,65 +83,79 @@ function simplify(obj) {
         } else {
             return "";
         }
-   } else if (Object.prototype.toString.call(obj) == "[object Object]") {
-       var out = {};
-       for (var property in obj) {
-           if( property === 'RESPONSE')
-               outProperty = property.toLowerCase();
-           else {
-               outProperty = property[0].toLowerCase();
-               outProperty = outProperty + property.substr(1, property.length);
-           }
-           out[outProperty] = simplify(obj[property]);
-       }
+    } else if (Object.prototype.toString.call(obj) == "[object Object]") {
+        var out = {};
+        for (var property in obj) {
+            if (property === 'RESPONSE') 
+                outProperty = property.toLowerCase();
+             else {
+                outProperty = property[0].toLowerCase();
+                outProperty = outProperty + property.substr(1, property.length);
+            } out[outProperty] = simplify(obj[property]);
+        }
         return out;
     } else {
         return obj;
     }
 }
 
-
-function normalise(obj, pkey, pret){      
-        ret = {};
-        for(var key in obj){             
-            if (typeof obj[key] === 'object') {
-                console.log("o  "+pkey + " " + key);
-                if( key !== "$" ) {
-                    if (ret === undefined ) ret = {}
-                    if(pkey === undefined) {                                   
-                        console.log(">", key, ret);
-                        ret[key] = normalise(obj[key], key,   ret);
-                        console.log("<", key, ret);
-                    } else {
-                        ret[pkey] = {};
-                        console.log("+", ret);
-                        //ret[pkey][key] = normalise(obj[key],  key,  ret);
-                    }                    
-                } else {
-                    console.log("$", pret);
-                    ret = pret;                    
-                    pret[key] = normalise(obj[key], pkey, ret);
-                }                
+function normalise(obj, pkey, pret) {
+    if (typeof obj === 'object') {
+        var out = {};
+        for (var property in obj) {
+            console.log(">>>", property, obj[property]);
+            console.log("$$$", obj['$']);
+            if (obj['$'] !== undefined) { // out[property] = normalise(obj[property]);
+                out = obj['$'];
             } else {
-                console.log("v  "+pkey + " " + key + ": " + obj[key]);
-                
-                var skey = key;
-                //if( key === "_")  skey = "result";                
-                if( key !== "$" ) { ret[skey] = obj[key]; }
-                console.log(ret);
+                out[property] = normalise(obj[property]);
             }
+
         }
-        return ret;
-} 
+        return out;
+    } else { // console.log("###", obj);
+        return obj;
+    }
+}
+
+function transform_oree(aobj) {
+    var obj = aobj['RESPONSE'];
+    var out = {};    
+    for (var property in obj) {
+        if (property === '$') {
+            for (var key in obj[property]) {
+                out[key] = obj[property][key];
+            }
+        } else {
+            out[property] = {};
+            for (var key in obj[property]) {
+                var okey = key;
+                if (key === '$') {
+                    for (var akey in obj[property][key]) {
+                        out[property][akey] = obj[property][key][akey];
+                    }
+                } else {
+                    if(key == '_') okey = 'result';                
+                    out[property][okey] = obj[property][key]; 
+                }
+            }
+            
+        }
+    }
+    var ret = {
+        response : out
+    }
+    return ret;
+}
 
 
 // MAIN --------------------------------------------------------------------------------
 var resn = require('./oree-no.json');
 var out = {};
 var oout;
-oout = normalise( resn );
-//console.log( JSON.stringify(out, null, 4) );
-console.log( oout );
+oout = transform_oree(resn);
+console.log(JSON.stringify(oout, null, 4));
+// console.log(oout);
 /*
 var reso = require('./oree-ok.json');
 out = {};
