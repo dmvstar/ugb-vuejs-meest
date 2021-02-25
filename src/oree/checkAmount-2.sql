e7bc0ee1d82b99c83a1f83e58df685bb871162d4
StarDust <dmvstar.devel@gmail.com>
2021-02-26 01:36:52 +0200

    Update Add tests + mono

--- src/oree/checkAmount-2.sql
+++ src/oree/checkAmount-2.sql
@@ -1,130 +1,265 @@
- Declare @DocInfo VarChar(120),@MainAmount money,@Status int
+DECLARE @DocInfo VARCHAR(120), @MainAmount MONEY, @Status INT, @MaxAmountDay MONEY, @MaxAmountWeek MONEY, @MaxAmountMonth MONEY;
+--SET @DocInfo = 'Македон Євгеній Віталійович;;;;,;;;;';
+--SET @MainAmount = 500000;
+SET @DocInfo='{{payload.docInfo}}'
+SET @MainAmount='{{payload.mainAmount}}'
 
-SET @DocInfo='Македон Євгеній Віталійович;;;;,;;;;'
-SET @MainAmount=399701
-SET @Status=1
-
-CREATE TABLE #rez(Id int, crncyamount money,mainamount money)
-
-CREATE TABLE #rez1(Id int, crncyamount money,mainamount money)
-
-CREATE TABLE #rez2(Id int, crncyamount money,mainamount money)
-
-CREATE TABLE #rez3(Type_ varchar(6),Status_ int,MainAmount money)
-
-INSERT INTO #rez(Id,mainamount)
-SELECT  b.Id
-       ,b.mainamount
-FROM bills b with 
-(nolock
+SET @Status = 1;
+SET @MaxAmountDay = 400000;
+SET @MaxAmountWeek = 800000;
+SET @MaxAmountMonth = 3200000;
+CREATE TABLE #rez
+(Id          INT, 
+ crncyamount MONEY, 
+ mainamount  MONEY
+);
+CREATE TABLE #rez1
+(Id          INT, 
+ crncyamount MONEY, 
+ mainamount  MONEY
+);
+CREATE TABLE #rez2
+(Id          INT, 
+ crncyamount MONEY, 
+ mainamount  MONEY
+);
+CREATE TABLE #rez3
+(Type_      VARCHAR(6), 
+ MaxAmount_ MONEY, 
+ Status_    INT, 
+ MainAmount MONEY
+);
+INSERT INTO #rez
+(Id
+, mainamount
 )
-JOIN sv_RepoParams sv
-ON sv.OwnerId=b.id AND sv.TaskCode='bills' AND sv.ParamCode='DocInfo'
-WHERE b.ActionId=336 
-AND ( ( 0=1 OR ( ProcessFlag & 3 = 1 ) OR ( ProcessFlag & 3 = 3 ) ) ) 
-AND sv.Param=@DocInfo 
-AND b.DayDate=datediff(day,0,getdate()) 
-
-INSERT INTO #rez1(Id,mainamount)
-SELECT  b.Id
-       ,b.mainamount
-FROM bills b with 
-(nolock
+       SELECT b.Id
+            , b.mainamount
+       FROM bills b WITH(NOLOCK)
+            JOIN sv_RepoParams sv ON sv.OwnerId = b.id
+                                     AND sv.TaskCode = 'bills'
+                                     AND sv.ParamCode = 'DocInfo'
+       WHERE b.ActionId = 336
+             AND ((0 = 1
+                   OR (ProcessFlag&3 = 1)
+                   OR (ProcessFlag&3 = 3)))
+             AND sv.Param = @DocInfo
+             AND b.DayDate = DATEDIFF(day, 0, GETDATE());
+INSERT INTO #rez1
+(Id
+, mainamount
 )
-JOIN sv_RepoParams sv
-ON sv.OwnerId=b.id AND sv.TaskCode='bills' AND sv.ParamCode='DocInfo'
-WHERE b.ActionId=336 
-AND ( ( 0=1 OR ( ProcessFlag & 3 = 1 ) OR ( ProcessFlag & 3 = 3 ) ) ) 
-AND sv.Param=@DocInfo 
-AND b.DayDate=datediff(day,0,getdate())-7 
-
-INSERT INTO #rez2(Id,mainamount)
-SELECT  b.Id
-       ,b.mainamount
-FROM bills b with 
-(nolock
+       SELECT b.Id
+            , b.mainamount
+       FROM bills b WITH(NOLOCK)
+            JOIN sv_RepoParams sv ON sv.OwnerId = b.id
+                                     AND sv.TaskCode = 'bills'
+                                     AND sv.ParamCode = 'DocInfo'
+       WHERE b.ActionId = 336
+             AND ((0 = 1
+                   OR (ProcessFlag&3 = 1)
+                   OR (ProcessFlag&3 = 3)))
+             AND sv.Param = @DocInfo
+             AND b.DayDate = DATEDIFF(day, 0, GETDATE()) - 7;
+INSERT INTO #rez2
+(Id
+, mainamount
 )
-JOIN sv_RepoParams sv
-ON sv.OwnerId=b.id AND sv.TaskCode='bills' AND sv.ParamCode='DocInfo'
-WHERE b.ActionId=336 
-AND ( ( 0=1 OR ( ProcessFlag & 3 = 1 ) OR ( ProcessFlag & 3 = 3 ) ) ) 
-AND sv.Param=@DocInfo 
-AND b.DayDate=datediff(day,0,getdate())-30 If isnull(( 
-SELECT  COUNT(1)
-FROM #rez),0)<>0 Begin If (
-SELECT  SUM(mainamount)
-FROM #rez)<400000 begin If ((
-SELECT  SUM(mainamount)
-FROM #rez)+@MainAmount)<=400000 Begin
-
-SET @Status=1 End Else Begin
-SET @MainAmount=400000-(
-SELECT  SUM(mainamount)
-FROM #rez)-@MainAmount
-
-SET @Status=1 End end else Begin
-SET @MainAmount=0
-SET @Status=0 end end If @MainAmount<0 Begin
-SET @MainAmount=0
-SET @Status=0 end
-
-INSERT INTO #rez3(Type_,Status_,MainAmount)
-SELECT  'Day'
-       ,@Status
-       ,@MainAmount
-
-DROP TABLE #rez If isnull((
-SELECT  COUNT(1)
-FROM #rez1),0)<>0 Begin If (
-SELECT  SUM(mainamount)
-FROM #rez1)<800000 begin If ((
-SELECT  SUM(mainamount)
-FROM #rez1)+@MainAmount)<=800000 Begin
-
-SET @Status=1 End Else Begin
-SET @MainAmount=800000-(
-SELECT  SUM(mainamount)
-FROM #rez1)-@MainAmount
-
-SET @Status=1 End end else Begin
-SET @MainAmount=0
-SET @Status=0 end end If @MainAmount<0 Begin
-SET @MainAmount=0
-SET @Status=0 end
-
-INSERT INTO #rez3(Type_,Status_,MainAmount)
-SELECT  'week'
-       ,@Status
-       ,@MainAmount
-
-DROP TABLE #rez1 If isnull((
-SELECT  COUNT(1)
-FROM #rez2),0)<>0 Begin If (
-SELECT  SUM(mainamount)
-FROM #rez2)<3200000 begin If ((
-SELECT  SUM(mainamount)
-FROM #rez2)+@MainAmount)<=3200000 Begin
-
-SET @Status=1 End Else Begin
-SET @MainAmount=3200000-(
-SELECT  SUM(mainamount)
-FROM #rez2)-@MainAmount
-
-SET @Status=1 End end else Begin
-SET @MainAmount=0
-SET @Status=0 end end If @MainAmount<0 Begin
-SET @MainAmount=0
-SET @Status=0 end
-
-INSERT INTO #rez3(Type_,Status_,MainAmount)
-SELECT  'month'
-       ,@Status
-       ,@MainAmount
-
-DROP TABLE #rez2
-SELECT  Type_
-       ,Status_
-       ,MainAmount
-FROM #rez3
-
-DROP TABLE #rez3
+       SELECT b.Id
+            , b.mainamount
+       FROM bills b WITH(NOLOCK)
+            JOIN sv_RepoParams sv ON sv.OwnerId = b.id
+                                     AND sv.TaskCode = 'bills'
+                                     AND sv.ParamCode = 'DocInfo'
+       WHERE b.ActionId = 336
+             AND ((0 = 1
+                   OR (ProcessFlag&3 = 1)
+                   OR (ProcessFlag&3 = 3)))
+             AND sv.Param = @DocInfo
+             AND b.DayDate = DATEDIFF(day, 0, GETDATE()) - 30;
+IF ISNULL(
+(
+    SELECT COUNT(1)
+    FROM #rez
+), 0) <> 0
+    BEGIN
+        IF
+        (
+            SELECT SUM(mainamount)
+            FROM #rez
+        ) < @MaxAmountDay
+            BEGIN
+                IF(
+                (
+                    SELECT SUM(mainamount)
+                    FROM #rez
+                ) + @MainAmount) <= @MaxAmountDay
+                    BEGIN
+                        SET @Status = 1;
+                END;
+                    ELSE
+                    BEGIN
+                        SET @MainAmount = @MaxAmountDay -
+                        (
+                            SELECT SUM(mainamount)
+                            FROM #rez
+                        ) - @MainAmount;
+                        SET @Status = 1;
+                END;
+        END;
+            ELSE
+            BEGIN
+                SET @MainAmount = 0;
+                SET @Status = 0;
+        END;
+END;
+ELSE 
+BEGIN
+    IF(@MainAmount > @MaxAmountDay)
+    BEGIN
+        SET @MainAmount = 0;
+        SET @Status = 0;
+    END;
+END;
+IF @MainAmount < 0
+    BEGIN
+        SET @MainAmount = 0;
+        SET @Status = 0;
+END;
+INSERT INTO #rez3
+(Type_
+, MaxAmount_
+, Status_
+, MainAmount
+)
+       SELECT 'Day'
+            , @MaxAmountDay
+            , @Status
+            , @MainAmount;
+DROP TABLE #rez;
+IF ISNULL(
+(
+    SELECT COUNT(1)
+    FROM #rez1
+), 0) <> 0
+    BEGIN
+        IF
+        (
+            SELECT SUM(mainamount)
+            FROM #rez1
+        ) < @MaxAmountWeek
+            BEGIN
+                IF(
+                (
+                    SELECT SUM(mainamount)
+                    FROM #rez1
+                ) + @MainAmount) <= @MaxAmountWeek
+                    BEGIN
+                        SET @Status = 1;
+                END;
+                    ELSE
+                    BEGIN
+                        SET @MainAmount = @MaxAmountWeek -
+                        (
+                            SELECT SUM(mainamount)
+                            FROM #rez1
+                        ) - @MainAmount;
+                        SET @Status = 1;
+                END;
+        END;
+            ELSE
+            BEGIN
+                SET @MainAmount = 0;
+                SET @Status = 0;
+        END;
+END;
+ELSE 
+BEGIN
+    IF(@MainAmount > @MaxAmountWeek)
+    BEGIN
+        SET @MainAmount = 0;
+        SET @Status = 0;
+    END;
+END;
+IF @MainAmount < 0
+    BEGIN
+        SET @MainAmount = 0;
+        SET @Status = 0;
+END;
+INSERT INTO #rez3
+(Type_
+, MaxAmount_
+, Status_
+, MainAmount
+)
+       SELECT 'week'
+            , @MaxAmountWeek
+            , @Status
+            , @MainAmount;
+DROP TABLE #rez1;
+IF ISNULL(
+(
+    SELECT COUNT(1)
+    FROM #rez2
+), 0) <> 0
+    BEGIN
+        IF
+        (
+            SELECT SUM(mainamount)
+            FROM #rez2
+        ) < @MaxAmountMonth
+            BEGIN
+                IF(
+                (
+                    SELECT SUM(mainamount)
+                    FROM #rez2
+                ) + @MainAmount) <= @MaxAmountMonth
+                    BEGIN
+                        SET @Status = 1;
+                END;
+                    ELSE
+                    BEGIN
+                        SET @MainAmount = @MaxAmountMonth -
+                        (
+                            SELECT SUM(mainamount)
+                            FROM #rez2
+                        ) - @MainAmount;
+                        SET @Status = 1;
+                END;
+        END;
+            ELSE
+            BEGIN
+                SET @MainAmount = 0;
+                SET @Status = 0;
+        END;
+END;
+ELSE 
+BEGIN
+    IF(@MainAmount > @MaxAmountMonth)
+    BEGIN
+        SET @MainAmount = 0;
+        SET @Status = 0;
+    END;
+END;
+IF @MainAmount < 0
+    BEGIN
+        SET @MainAmount = 0;
+        SET @Status = 0;
+END;
+INSERT INTO #rez3
+(Type_
+, MaxAmount_
+, Status_
+, MainAmount
+)
+       SELECT 'month'
+            , @MaxAmountMonth
+            , @Status
+            , @MainAmount;
+DROP TABLE #rez2;
+SELECT Type_ AS type
+     , MaxAmount_ AS maxAmount
+     , Status_ AS status
+     , MainAmount AS mainAmount
+FROM #rez3;
+DROP TABLE #rez3;
