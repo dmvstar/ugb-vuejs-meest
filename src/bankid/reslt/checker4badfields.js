9c3efcd11f5783d49c0d6897a4087fbc041f824b
StarDust <dmvstar.devel@gmail.com>
2021-03-19 10:19:09 +0200

    Update test + checker

--- src/bankid/reslt/checker4badfields.js
+++ src/bankid/reslt/checker4badfields.js
@@ -1,7 +1,15 @@
 //@what checker4badfields.js
 
 var error = false;
+
 var regDate = "^(0[1-9]|[12][0-9]|3[01])[.](0[1-9]|1[012])[.](19|20)[0-9]{2}$"
+var regSD10  = "^[0-9]{10}$"
+var regSD09  = "^[0-9]{9}$"
+var regSPass = "^[А-Ю][А-Ю][0-9]{6}$"
+var regSAddr = "^(factual|juridical)$";
+var regSDocs = "^(passport|idpassport)$";
+var regSDocsAll = "^(passport|idpassport|zpassport|ident)$";
+
 var checkDate = "";
 
 var mess = "У Клиента ["+
@@ -9,6 +17,17 @@ msg.in.bankid_cli.person.inn+"]["
 +msg.in.bankid_cli.person.lastName+" "
 +msg.in.bankid_cli.person.firstName + 
 +msg.in.bankid_cli.person.middleName+"]";
+
+const regD09 = new RegExp(regSD09, "gm");
+const regPass = new RegExp(regSPass, "gm");
+var checkPass = false;
+// inn -> '0000000000'
+if( regPass.test(msg.in.bankid_cli.person.inn) | 
+    regD09.test(msg.in.bankid_cli.person.inn)  )
+{
+    checkPass = true;
+    msg.in.bankid_cli.person.inn = '0000000000';
+}
 
 if( msg.in.bankid_cli.addresses.length === 0 ) {
     msg.errorCodeRe = 1430;
@@ -27,12 +46,23 @@ if( msg.in.bankid_cli.documents.length === 0 ) {
         const regexpDate = new RegExp(regDate, "gm");
         if( !regexpDate.test(d.dateIssue) ){
             msg.errorCodeRe = 1441;
-            mess += " Ошибка формата поля dateIssue " + d.dateIssue + 
+            mess += " Ошибка формата поля документа dateIssue " + d.dateIssue + 
             " ("+regexpDate.test(d.dateIssue)+")"
             msg.payload.result = mess;
             error = true;
             break;
         }
+
+        const regDocs = new RegExp(regSDocs, "gm");
+        if( !regDocs.test(d.type) ){
+            msg.errorCodeRe = 1442;
+            mess += " Ошибка формата поля документа type " + d.type + 
+            " ("+regDocs.test(d.type)+")"
+            msg.payload.result = mess;
+            error = true;
+            break;
+        }
+
     }
 }
 
--- src/bankid/reslt/tdate.js
+++ src/bankid/reslt/tdate.js
@@ -46,15 +46,63 @@ var regDate = "^[0-3][0-9].[0-2][0-9].[1-2][0-9][0-9][0-9]"
 regDate = "^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$ "
 regDate = "^(0[1-9]|[12][0-9]|3[01])[.](0[1-9]|1[012])[.](19|20)[0-9]{2}$"
 
-
 for(d of dts) {
     const regexpDate = new RegExp(regDate, "gm")
     console.log(d, regexpDate.test(d))
 }
 
+var regSD10  = "^[0-9]{10}$"
+var regSD09  = "^[0-9]{9}$"
+var regSPass = "^[А-Ю][А-Ю][0-9]{6}$"
+var regSAddr = "^(factual|juridical)$";
+var regSDocs = "^(passport|idpassport)$";
 
+var digs = [
+    "9876765656",
+    "987676565",
+    "АВ123456",
+    "АВ123456 ",
+    "98767656561",
+    "АВ1234561",
+    "A876765656"
+]
 
+console.log('----------------- regSD10')
 
+for(d of digs) {
+    const regexp = new RegExp(regSD10, "gm");
+    console.log(d, regexp.test(d));
+}
+
+console.log('----------------- regSD09')
+
+for(d of digs) {
+    const regexp = new RegExp(regSD09, "gm");
+    console.log(d, regexp.test(d));
+}
+
+console.log('----------------- regSPass')
+
+for(d of digs) {
+    const regexp = new RegExp(regSPass, "gm");
+    console.log(d, regexp.test(d));
+}
+
+console.log('-----------------')
+
+var regexpTest;
+var atypes = ['factual','juridical'];
+regexpTest = new RegExp(regSAddr, "gm");
+console.log('factual', regexpTest.test('factual'));
+console.log('2factual', regexpTest.test('2factual'));
+console.log('factual', regexpTest.test('factual'));
+
+console.log('-----------------')
+
+var dtypes = ['passport','idpassport','zpassport','ident'];
+regexpTest = new RegExp(regSDocs, "gm");
+console.log('passport', regexpTest.test('passport'));
+console.log('ксива', regexpTest.test('ксива'));
 
 
 
