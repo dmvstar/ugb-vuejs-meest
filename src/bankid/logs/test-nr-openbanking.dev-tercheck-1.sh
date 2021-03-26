af52e73310eec2bf1e62a164db3b3af3bf3d2349
StarDust <dmvstar.devel@gmail.com>
2021-03-26 11:06:38 +0200

    update tercheck get example

--- src/bankid/logs/test-nr-openbanking.dev-tercheck-1.sh
+++ src/bankid/logs/test-nr-openbanking.dev-tercheck-1.sh
@@ -4,12 +4,12 @@ curl -XPOST \
 -H "Content-type:application/json" \
 -H "x-auth-token:PTzQlEIYZVslkOyzKh41cJCfJCSuhJJ8" \
 -d @tercheck-1.json \
-https://nr-openbanking.ukrgasaws.com/openbanking/bankid/client/tercheck 2>/dev/null | jq
+https://nr-openbanking.dev.ukrgasaws.com/openbanking/bankid/client/tercheck 2>/dev/null | jq
 
 
 curl -XPOST \
 -H "Content-type:application/json" \
 -H "x-auth-token:PTzQlEIYZVslkOyzKh41cJCfJCSuhJJ8" \
 -d @tercheck-2.json \
-https://nr-openbanking.ukrgasaws.com/openbanking/bankid/client/tercheck 2>/dev/null | jq
+https://nr-openbanking.dev.ukrgasaws.com/openbanking/bankid/client/tercheck 2>/dev/null | jq
 
