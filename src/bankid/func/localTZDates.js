21dcc5651465107434e3a0e6a578791ef5903bf3
StarDust <dmvstar.devel@gmail.com>
2021-03-31 22:42:18 +0300

    Update test + func

--- src/bankid/func/localTZDates.js
+++ src/bankid/func/localTZDates.js
@@ -1,5 +1,3 @@
-var dt = new Date();
-
 function localTZDates(zdate){
     // 2021-03-31T10:46:48.841Z
     var ldate=new Date(zdate.getTime() - (60000*(zdate.getTimezoneOffset()-2)));
@@ -22,6 +20,7 @@ function localTZDates(zdate){
     };
 }
 
+var dt = new Date();
 msg.payload = {
     zdate : dt.toISOString(),
     ldate : localTZDates(dt)
