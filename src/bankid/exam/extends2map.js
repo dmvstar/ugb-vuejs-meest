d7c836444c735adae2c8f9420e6c3f8c8774015b
StarDust <dmvstar.devel@gmail.com>
2021-04-16 11:43:53 +0300

    Update test + examples + maps

---
+++ src/bankid/exam/extends2map.js
@@ -0,0 +1,15 @@
+var validateData = require('./kk-client-info-0.json');
+
+var ext = validateData.extends;
+var out = {};
+
+for(var o in ext){
+    //console.log(o);
+    out[o] = {
+            "needed": false,
+            "express": "^[1-9]$",
+            "message": "Ошибка формата поля " + o
+        }
+}
+
+console.log(JSON.stringify(out, null, 2));
