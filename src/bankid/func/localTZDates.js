1a80f3246d2c23b562ed465bfd5c5c3c9f11c1b8
StarDust <dmvstar.devel@gmail.com>
2021-04-19 18:07:35 +0300

    Update test + examples + maps + adddocs

--- src/bankid/func/localTZDates.js
+++ src/bankid/func/localTZDates.js
@@ -2,7 +2,7 @@ function localTZDates(zdate){
     // 2021-03-31T10:46:48.841Z
     var tzShift = -2;
     var ldate=new Date(zdate.getTime() - (60000*(zdate.getTimezoneOffset()+tzShift)));
-    var ldateTime = ldate.toISOString().replace('Z', '');
+    var ldateTime = ldate.toISOString();//'.replace('Z', '');
     
     var ldate = ldateTime.substring(8,10)+'.'+
               ldateTime.substring(5,7)+'.'+
@@ -13,6 +13,8 @@ function localTZDates(zdate){
               ldateTime.substring(17)
               
     return {
+        requestRef: Math.floor(100000 + Math.random() * 900000),
+        num4: Math.floor(1000 + Math.random() * 9000),
         zeroDateTime: zdate.toISOString(),
         localDate: ldate,
         localTime: ltime,
@@ -26,4 +28,5 @@ msg.payload = {
     zdate : dt.toISOString(),
     ldate : localTZDates(dt)
 };
+msg.payload = localTZDates(dt);
 return msg;
