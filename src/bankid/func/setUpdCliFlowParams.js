cb15581201c93f34a95bb3cb4ac0ecd9c1135b43
StarDust <dmvstar.devel@gmail.com>
2021-06-20 09:14:44 +0300

    Update set flow params ext

---
+++ src/bankid/func/setUpdCliFlowParams.js
@@ -0,0 +1,38 @@
+/**
+ * @WHERE setUpdCliFlowParams.js
+ * @WHAT Установка парасетров для флоу 
+ */
+
+msg.workDir = 'LOC';
+if(env.get("ENVIRONMENT") !== undefined) 
+    msg.workDir = env.get("ENVIRONMENT").substring(0,3).toUpperCase();
+
+let reqs = {};
+
+if (msg.workDir == "PRO") {
+    reqs['Webbank/Api/ClientApi/ModifyClientFL'] = {
+        headers : { 'Content-Type': 'application/json', 
+                    'Host' : 'kv00-v-appfe-pr.ukrgas.bank.local'},
+        url : 'https://10.13.97.8/Webbank/Api/ClientApi/ModifyClientFL'
+    };   
+    reqs['Webbank/Api/ClientApi/FindClientFL'] = {
+        headers : { 'Content-Type': 'application/json', 
+                    'Host' : 'app-front-pp.ukrgasbank.com'},
+        url : 'https://10.13.97.8/Webbank/Api/ClientApi/FindClientFL'
+    };
+} else if (msg.workDir == "DEV") {
+    reqs['Webbank/Api/ClientApi/ModifyClientFL'] = {
+        headers : { 'Content-Type': 'application/json', 
+                    'Host' : 'app-front-pp.ukrgasbank.com'},
+        url : 'https://10.13.97.10/Webbank/Api/ClientApi/ModifyClientFL'
+    };   
+    reqs['Webbank/Api/ClientApi/FindClientFL'] = {
+        headers : { 'Content-Type': 'application/json', 
+                    'Host' : 'app-front-pp.ukrgasbank.com'},
+        url : 'https://10.13.97.10/Webbank/Api/ClientApi/FindClientFL'
+    };          
+} 
+
+flow.set("reqs", reqs);
+msg.payload = reqs;
+return msg;
