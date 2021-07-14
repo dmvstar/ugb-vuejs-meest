5dd8c3877a96a7c0873b66bbc4d9b7e8a94760e7
StarDust <dmvstar.devel@gmail.com>
2021-07-14 15:14:51 +0300

    Update mapping ext++

--- src/bankid/tests/calculate.js
+++ src/bankid/tests/calculate.js
@@ -1,2 +1,34 @@
-console.log(eval('2 + 2'));
-console.log(eval('2 === 2'));
+var body = {
+    main :{
+        f1 : 0,
+        f2 : 1
+        },
+    ext : {
+        maybe0 : true,
+        maybe1 : "{{main.f1}}===1",
+        maybe2 : "{{main.f2}}===1"
+    }
+}
+
+//console.log(eval('2 + 2'));
+//console.log(eval('2 === 2'));
+
+function isNeeds(root, value) 
+{
+    ret = value;
+    console.log(0);
+    console.log(0, value, typeof value === 'string');
+    if(typeof value === 'string') {
+        var mustache = require("mustache");
+        var output = mustache.render(value, root);
+        console.log(1, value, output);
+        var ret = false;
+        ret = eval(output);
+    }
+    console.log(2, value, ret);
+    return ret;
+}
+console.log(body);
+isNeeds(body, body.ext.maybe0);
+isNeeds(body, body.ext.maybe1);
+isNeeds(body, body.ext.maybe2);
